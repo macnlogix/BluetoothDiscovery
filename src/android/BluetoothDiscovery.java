fbb106c8e87ddcd5941b7d8fd092170da26ed7ad
macnlogix <m.chishti@tahaluf.ae>
2020-04-25 20:30:00 +0400

    Added Discovery

--- src/android/BluetoothDiscovery.java
+++ src/android/BluetoothDiscovery.java
@@ -1,6 +1,8 @@
 package cordova.plugin.bluetooth.discovery;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothManager;
 import android.content.pm.PackageManager;
 
 import android.app.Activity;
@@ -10,9 +12,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
@@ -56,6 +60,8 @@ public class BluetoothDiscovery extends CordovaPlugin {
     private static final String SET_NAME = "setName";
     private static final String SET_DISCOVERABLE = "setDiscoverable";
 
+    private final String logOperationUnsupported = "Operation unsupported";
+
     private CallbackContext enableBluetoothCallback;
     private CallbackContext deviceDiscoveredCallback;
 
@@ -66,6 +72,9 @@ public class BluetoothDiscovery extends CordovaPlugin {
     private static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final String ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int CHECK_PERMISSIONS_REQ_CODE = 2;
+    private final int REQUEST_ACCESS_COARSE_LOCATION = 59628;
+    private final int REQUEST_LOCATION_SOURCE_SETTINGS = 59629;
+
     private CallbackContext permissionCallback;
 
     //Object keys
@@ -106,6 +115,8 @@ public class BluetoothDiscovery extends CordovaPlugin {
     private final String keyConnectionPriority = "connectionPriority";
     private final String keyMtu = "mtu";
 
+
+
     private CallbackContext initCallbackContext;
     private CallbackContext scanCallbackContext;
     private CallbackContext permissionsCallback;
@@ -117,7 +128,7 @@ public class BluetoothDiscovery extends CordovaPlugin {
         LOG.d(TAG, "action = " + action);
 
         if (bluetoothAdapter == null) {
-            bluetoothAdapter = getAdapter()
+            bluetoothAdapter = getAdapter();
         }
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
@@ -127,24 +138,86 @@ public class BluetoothDiscovery extends CordovaPlugin {
         if (action.equals("getAdapterInfo")) {
             this.getAdapterInfoAction(callbackContext);
             return true;
+        }else if ("hasPermission".equals(action)) {
+            hasPermissionAction(callbackContext);
+            return true;
+        }else if ("scanDevices".equals(action)) {
+            this.scanDevices(callbackContext);
+            return true;
+        } else if ("requestPermission".equals(action)) {
+            requestPermissionAction(callbackContext);
+            return true;
+        } else if ("isLocationEnabled".equals(action)) {
+            isLocationEnabledAction(callbackContext);
+            return true;
+        } else if ("requestLocation".equals(action)) {
+            requestLocationAction(callbackContext);
+            return true;
         }
         return false;
     }
 
+    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Create a new device item
+                scanCallbackContext.success("{\"deviceName\":"+device.getName()+",\"deviceName\":"+device.getAddress()+"}");
+                Log.d(TAG, "mBroadcastReceiver2: Discoverability Enabled.--"+device.getName()+"---"+device.getAddress());
+            }
+        }
+    };
+
+    BroadcastReceiver scanModeReciver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+
+                int mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
+
+                switch (mode) {
+                    //Device is in Discoverable Mode
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability Enabled.");
+                        break;
+                    //Device not in discoverable mode
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability Disabled. Able to receive connections.");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_NONE:
+                        Log.d(TAG, "mBroadcastReceiver2: Discoverability Disabled. Not able to receive connections.");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTING:
+                        Log.d(TAG, "mBroadcastReceiver2: Connecting....");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTED:
+                        Log.d(TAG, "mBroadcastReceiver2: Connected.");
+                        break;
+                }
+
+            }
+        }
+
+    };
+
+    @SuppressLint("LongLogTag")
     private BluetoothAdapter getAdapter(){
         BluetoothAdapter mBluetoothAdapter = null;
          if (cordova.getActivity().checkCallingOrSelfPermission(Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED) {
             BluetoothManager bm;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                bm = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-                    mBluetoothAdapter = bm.getAdapter();
-                    } else {
-                        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-                    }
-                    Log.i("TEST", mBluetoothAdapter.getAddress());
-            if(mBluetoothAdapter.isEnabled()){
-                Log.i("BluetoothAdapter---isEnabled--", mBluetoothAdapter.isEnabled()+"");
+                bm = (BluetoothManager) cordova.getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+                mBluetoothAdapter = bm.getAdapter();
+            } else {
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             }
+            //Log.i("TEST", mBluetoothAdapter.getAddress());
+//            if(mBluetoothAdapter.isEnabled()){
+//                Log.i("BluetoothAdapter---isEnabled--", mBluetoothAdapter.isEnabled()+"");
+//            }
         }
         return mBluetoothAdapter;
     }
@@ -175,6 +248,117 @@ public class BluetoothDiscovery extends CordovaPlugin {
         return;
     }
 
+    public void discoverableEnable() {
+        Log.d(TAG, "btnEnableDisable_Discoverable: Making device discoverable for 300 seconds.");
+
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        cordova.getActivity().startActivity(discoverableIntent);
+
+        IntentFilter intentFilter = new IntentFilter(bluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        cordova.getActivity().registerReceiver(scanModeReciver,intentFilter);
+    }
+
+    private void scanDevices(CallbackContext callbackContext) {
+        Log.d(TAG, "btnDiscover: Looking for unpaired devices.");
+        scanCallbackContext = callbackContext;
+        if(bluetoothAdapter.isDiscovering()){
+            bluetoothAdapter.cancelDiscovery();
+            Log.d(TAG, "btnDiscover: Canceling discovery.");
+
+            //check BT permissions in manifest
+            checkBTPermissions();
+
+            bluetoothAdapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            cordova.getActivity().registerReceiver(bReciever, discoverDevicesIntent);
+        }
+        if(!bluetoothAdapter.isDiscovering()){
+
+            //check BT permissions in manifest
+            checkBTPermissions();
+
+            bluetoothAdapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            cordova.getActivity().registerReceiver(bReciever, discoverDevicesIntent);
+        }
+    }
+
+    public void hasPermissionAction(CallbackContext callbackContext) {
+        JSONObject returnObj = new JSONObject();
+
+        addProperty(returnObj, "hasPermission", (cordova.hasPermission(ACCESS_COARSE_LOCATION) && cordova.hasPermission(ACCESS_FINE_LOCATION)));
+
+        callbackContext.success(returnObj);
+    }
+
+    public void requestPermissionAction(CallbackContext callbackContext) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            JSONObject returnObj = new JSONObject();
+            addProperty(returnObj, keyError, "requestPermission");
+            addProperty(returnObj, keyMessage, logOperationUnsupported);
+            callbackContext.error(returnObj);
+            return;
+        }
+
+        permissionsCallback = callbackContext;
+        cordova.requestPermission(this, REQUEST_ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION);
+    }
+
+    private void isLocationEnabledAction(CallbackContext callbackContext) {
+        JSONObject returnObj = new JSONObject();
+
+        addProperty(returnObj, "isLocationEnabled", isLocationEnabled());
+
+        callbackContext.success(returnObj);
+    }
+
+    private boolean isLocationEnabled() {
+        boolean result = true;
+
+        //Only applies to Android 6.0, which requires the users to have location services enabled to scan for devices
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                result = (Settings.Secure.getInt(cordova.getActivity().getContentResolver(), Settings.Secure.LOCATION_MODE) != Settings.Secure.LOCATION_MODE_OFF);
+            } catch (Settings.SettingNotFoundException e) {
+                result = true; //Probably better to default to true
+            }
+        }
+
+        return result;
+    }
+
+    private void requestLocationAction(CallbackContext callbackContext) {
+        locationCallback = callbackContext;
+
+        Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        cordova.startActivityForResult(this, intent, REQUEST_LOCATION_SOURCE_SETTINGS);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        if (permissionsCallback == null) {
+            return;
+        }
+
+        //Just call hasPermission again to verify
+        JSONObject returnObj = new JSONObject();
+
+        addProperty(returnObj, "requestPermission", cordova.hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION));
+
+        permissionsCallback.success(returnObj);
+    }
+
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = cordova.getActivity().checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += cordova.getActivity().checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+                cordova.getActivity().requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
 
     //General Helpers
     private void addProperty(JSONObject obj, String key, Object value) {
